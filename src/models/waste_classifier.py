a3e329ed418a010a2c27d30f7c418dab146cc309
tarah7 <kehna56164m@gmail.com>
2026-09-12 20:24:16 +0530

    Improve CNN architecture

--- src/models/waste_classifier.py
+++ src/models/waste_classifier.py
@@ -3,16 +3,16 @@ class WasteClassifier (nn.Module):
     def __init__(self):
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding=1),
+            nn.Conv2d(3, 64, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(16, 64, 3, padding=1),
+            nn.Conv2d(64, 128, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten()
         )
-        self.classifier=nn.Linear(64,6)
+        self.classifier=nn.Linear(128,6)
     def forward(self,x):
         x=self.features(x)
         x=self.classifier(x)
